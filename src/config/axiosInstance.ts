9c9923cde2602f75d92eb9e917711d15b9acf6a3
dw.office <dimaswahyu.official@gmail.com>
2024-12-17 11:16:10 +0700

    Connect to Local Backend

--- src/config/axiosInstance.ts
+++ src/config/axiosInstance.ts
@@ -5,8 +5,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const axiosInstance = axios.create({
   // baseURL: 'http://192.168.0.102:9001/api/v1',
   // baseURL: 'http://103.217.144.36:9001/api/v1',
-  baseURL: 'https://api.koneksi.co.id/api/v1',
+  // baseURL: 'https://api.koneksi.co.id/api/v1',
   // baseURL: 'https://10.0.63.144:9001/api/v1',
+  baseURL: 'http://10.0.63.223:9001/api/v1',
   timeout: 10000,
 });
 
