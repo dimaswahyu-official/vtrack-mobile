ba24611d7f4070ac20d86fbd367031ebcf6ffd4a
dw-dev <dimaswahyu.official@gmail.com>
2025-03-20 00:39:57 +0700

    [update] Api

--- src/config/axiosInstance.ts
+++ src/config/axiosInstance.ts
@@ -7,7 +7,7 @@ import useConstantStore from '../store/useConstantStore';
 
 const axiosInstance = axios.create({
 	// baseURL: BASE_URL,
-	baseURL: "https://api.koneksi.co.id/api/v1",
+	baseURL: "https://apimd.niaganusaabadi.co.id/api/v1",
 	timeout: 10000,
 });
 
